2b4ced4fe605c59b402ff53f6100b1aad29bcdb7
Bryce Dumas <bryce.dumas@gmail.com>
2020-04-16 15:35:38 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,3 +7,8 @@ end
 def sort_array_char_count (array)
   array.sort {|a,b| a.length <=> b.length}
 end
+def swap_elements_from_to (array, index, dest)
+  swap = array[index]
+  array[index] = array[dest]
+  array[dest] = swap
+end
