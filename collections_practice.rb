410e7d4a6f28f5a326365ebce5c81e0928696acf
Bryce Dumas <bryce.dumas@gmail.com>
2020-04-16 15:40:43 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -13,7 +13,5 @@ def swap_elements_from_to (array, index, dest)
   array[dest] = swap
 end
 def swap_elements (array)
-  index = 2
-  dest = 3
-  swap_elements_from_to (array, index, dest)
+  swap_elements_from_to (array, 2,3)
 end
